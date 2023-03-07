4b3dfa61340ba7617fa904031ca65a61e8bdcd0d
Mohamed Yousfi <m.yousfi96e@gmail.com>
2023-03-07 10:16:05 +0100

    process a reviewer

--- data_clean.py
+++ data_clean.py
@@ -6,10 +6,26 @@ data = json.load(f)
 
 
 for obj in data:
+    reviewers = obj['requested_reviewers']
+    reviewers_list_name = []
+    for reviewer in reviewers:
+        tmp = reviewer['login']
+        reviewers_list_name.append(tmp)
+    obj['requested_reviewers'] = reviewers_list_name
     comment_list = obj['review_comments']
     for comment in comment_list:
-        comment.pop('diff_hunk', None)
+        if comment['user'] in obj['requested_reviewers']:
+            comment['user'] = 'reviewer'
+        else:
+            comment['user'] = 'not a reviewer'
+    comment_list = obj['comments']
+    for comment in comment_list:
+        if comment['user'] in obj['requested_reviewers']:
+            comment['user'] = 'reviewer'
+        else:
+            comment['user'] = 'not a reviewer'
 
-with open("cleaned_data.json", "w") as outfile:
+
+with open("cleaned_data_reviewers.json", "w") as outfile:
     json.dump(data, outfile)
 
